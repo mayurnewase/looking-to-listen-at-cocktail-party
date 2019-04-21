399f1204287359a685a3861e6348a7f99358ccf9
mayur_newase <mayur@gmail.com>
2019-04-21 10:35:14 +0530

    bug fix

--- data/prepare_directory.sh
+++ data/prepare_directory.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-mkdir -p ./speaker_background_spectrograms
+mkdir -p data/speaker_background_spectrograms
 
-mkdir -p ./two_speakers_mix_spectrograms/
-mkdir -p ./two_speakers_mix_spectrograms/part_1
+mkdir -p data/two_speakers_mix_spectrograms/
+mkdir -p data/two_speakers_mix_spectrograms/part_1
 
-mkdir -p ./speaker_video_embeddings/
-mkdir -p ./speaker_video_embeddings/part_1
+mkdir -p data/speaker_video_embeddings/
+mkdir -p data/speaker_video_embeddings/part_1
 
-mkdir -p ./chatter_audios
-mkdir -p ./chatter_audios/part_1
-mkdir -p ./chatter_audios/part_2
-mkdir -p ./chatter_audios/part_3
+mkdir -p data/chatter_audios
+mkdir -p data/chatter_audios/part_1
+mkdir -p data/chatter_audios/part_2
+mkdir -p data/chatter_audios/part_3
 
-mkdir -p ./clean_audios
-mkdir -p ./videos
-mkdir -p ./frames
-mkdir -p ./pretrained_model
+mkdir -p data/clean_audios
+mkdir -p data/videos
+mkdir -p data/frames
+mkdir -p data/pretrained_model
 
-mkdir -p temp/chatter_audio
-mkdir -p temp/chatter_video
+mkdir -p data/temp/chatter_audio
+mkdir -p data/temp/chatter_video
 
 echo "Done setting up directories set"
 
-python3 chatter_download.py
-python3 chatter_slicer.py
+python3 data/chatter_download.py
+python3 data/chatter_slicer.py
 
-rm -rf temp/*
+rm -rf data/temp/*
 
 echo "Done downloading chatter_files from audioset...."
 echo "Now you can download avspeech dataset."
